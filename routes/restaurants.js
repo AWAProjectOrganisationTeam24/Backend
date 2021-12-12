82074fcbe389bd5d5c932d1e619b0e8a8a26642c
JuliePrazakova <80099262+JuliePrazakova@users.noreply.github.com>
2021-12-12 22:41:15 +0200

    Update restaurants.js

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,55 +1,88 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer')
+const path = require('path')
+const cors = require("cors");
+const bodyParser = require('body-parser');
 const sql = require('../db');
 
+router.use(cors());
+router.use(express.static("./public"))
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 
-//SHOW ORDERS
-router.get("/customer/:id",  (req, res) => {
-    sql.query("SELECT * FROM orders WHERE id_customer = ?", [req.params.id] , function (err, result) {
-        if (err) throw err;
-        res.send(result);
-    });
 
-});
-router.get("/restaurant/:id",  (req, res) => {
-    sql.query("SELECT * FROM orders WHERE id_restaurant = ?", [req.params.id] , function (err, result) {
-        if (err) throw err;
-        res.send(result);
-    });
-});
-
-//CUSTOMER CREATES NEW ORDER
-router.post('/add-order/:id_restaurant/:id', (req,res) => {
-    sql.query("INSERT INTO orders (id_restaurant, id_customer, price, time, date, status, content, paid) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-        [req.params.id_restaurant, req.params.id, req.body.price, req.body.time, req.body.date, req.body.status, req.body.content, req.body.paid]);
-    sql.query("UPDATE customer SET money  = ? WHERE id_customer = ?", [req.body.money, req.params.id]);
-});
-
-
-//EDITING ORDERS
-router.post('/edit-order/customer/:id', (req,res) => {
-        sql.query("UPDATE orders SET status = ? WHERE id_order = ?",
-        [ req.params.id, req.body.status]);
-});
-router.get('/edit-order/customer/:id', (req,res) => {
-    sql.query("SELECT * FROM orders WHERE id_order = ?",
-        [req.params.id] , function (err, result) {
+//RESTAURANT DEFAULT LIST
+router.get("/", function(req,res) {
+        sql.query("SELECT * FROM restaurant", function (err, result) {
             if (err) throw err;
             res.send(result);
         });
 });
 
-router.post('/edit-order/restaurant/:id', (req,res) => {
-    sql.query("UPDATE orders SET status = ? WHERE id_order = ?",
-        [req.body.status, req.params.id]);
+//RESTAURANTS IN SEARCHED CITY
+router.post("/",  function(req, res) {
+    sql.query("SELECT * FROM restaurant WHERE city = ?", [req.body.city] , function (err, result) {
+        if (err) throw err;
+        res.send(result);
+    });
 });
 
-router.get('/edit-order/restaurant/:id', (req,res) => {
-    sql.query("SELECT * FROM orders WHERE id_order = ?",
-        [req.params.id] , function (err, result) {
-            if (err) throw err;
-            res.send(result);
-        });
+
+/// ADD RESTAURANT
+// Use of Multer
+const storage = multer.diskStorage({
+    destination: (req, file, callBack) => {
+        callBack(null, './public/images/restaurants/')     // './public/images/' directory name where save the file
+    },
+    filename: (req, file, callBack) => {
+        callBack(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({
+    storage: storage
 });
+
+router.post('/add-restaurant/:id', upload.single('file'),(req,res) => {
+    if(!req.file) {
+            console.log("No file upload");
+        } else {
+            const imgsrc = req.file.filename;
+            sql.query("INSERT INTO restaurant (name, address, id_manager, city, image, type, openHr) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                [req.body.name, req.body.address, req.params.id, req.body.city, imgsrc, req.body.type, req.body.openHr], (err, result) => {
+                    if (err) throw err
+                    console.log("file uploaded")
+                });
+        }
+});
+
+//EDIT RESTAURANT
+router.post('/edit-restaurant/:id', upload.single('file'),(req,res) => {
+
+    if (!req.file) {
+        sql.query("UPDATE restaurant SET name = ?, address = ?, city = ?, type = ?, openHr = ? WHERE id_restaurant = ?",
+            [req.body.name, req.body.address, req.body.city, req.body.type, req.body.openHr, req.params.id], (err, result) => {
+                if (err) throw err
+                console.log("updated")
+            });
+    } else {
+        console.log(req.file.filename)
+        const imgsrc = 'http://127.0.0.1:3000/images/' + req.file.filename
+        sql.query("UPDATE restaurant SET name = ?, address = ?, city = ?, image = ?, type = ?, openHr = ? WHERE id_restaurant = ?",
+            [req.body.name, req.body.address, req.body.city, imgsrc, req.body.type, req.body.openHr, req.params.id], (err, result) => {
+                if (err) throw err
+                console.log("file updated")
+            });
+    }
+});
+
+router.get('/view-restaurant/:id', (req,res) => {
+    sql.query('SELECT * FROM restaurant WHERE id_manager = ?', [req.params.id], (err,result) => {
+        if (err) throw err
+        res.send(result);
+    })
+})
+
 module.exports = router;
